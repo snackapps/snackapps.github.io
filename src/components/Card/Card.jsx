6cf7a36ead0e27e5596bf24be9e02aed46587daa
snackware <snackware@example.com>
2026-02-12 20:21:09 +0500

    Update profile with banner and new components

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,5 @@
 import { useCardSpotlight } from '../../hooks/useCardSpotlight';
-import Avatar from '../Avatar/Avatar';
 import SocialLinks from '../SocialLinks/SocialLinks';
-import Quote from '../Quote/Quote';
 import styles from './Card.module.css';
 
 const Card = () => {
@@ -10,11 +8,16 @@ const Card = () => {
   return (
     <div className={styles.container}>
       <div ref={cardRef} className={styles.card}>
-        <Avatar />
+        <div className={styles.banner}>
+          <img 
+            src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ2F6Z284Y3J5ZmpxN2I3N3phZ2Jpc3loMWZnZHV5bWl4NGFiZzMycyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/RyrAgBKLtNdGU/giphy.gif" 
+            alt="Discord Banner"
+            className={styles.bannerImage}
+          />
+        </div>
         <h1 className={styles.name}>snackware</h1>
         <p className={styles.bio}>no name skid & vibecoder</p>
         <SocialLinks />
-        <Quote />
       </div>
     </div>
   );
